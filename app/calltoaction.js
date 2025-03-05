6910e38d2a35698305234160da39bb37270adc94
ingemateo30 <msalazar5@udi.edu.co>
2025-03-05 11:25:19 -0500

    whatsapp

--- app/calltoaction.js
+++ app/calltoaction.js
@@ -1,18 +1,22 @@
 const CallToAction = () => {
     return (
-      <section className="bg-[#fe9903] text-black py-20">
-        <div className="container mx-auto text-center">
-          <h2 className="text-4xl font-bold mb-6">
-            Transformemos Juntos tu Comunicación
-          </h2>
-          <p className="text-xl mb-8">
-            Contáctanos y descubre cómo podemos impulsar tu negocio
-          </p>
-          <button className="bg-black text-white px-8 py-3 rounded-full text-lg hover:bg-zinc-800 transition-colors">
-            Solicitar Consulta Gratuita
-          </button>
-        </div>
-      </section>
+        <section className="bg-[#fe9903] text-black py-20">
+            <div className="container mx-auto text-center">
+                <h2 className="text-4xl font-bold mb-6">
+                    Transformemos Juntos tu Comunicación
+                </h2>
+                <p className="text-xl mb-8">
+                    Contáctanos y descubre cómo podemos impulsar tu negocio
+                </p>
+                <button
+                    className="bg-black text-white px-8 py-3 rounded-full text-lg hover:bg-zinc-800 transition-colors"
+                    onClick={() => window.open("https://wa.me/573212631673", "_blank")}
+                >
+                    Solicitar Consulta Gratuita
+                </button>
+
+            </div>
+        </section>
     );
-  };
-  export default CallToAction;
+};
+export default CallToAction;
