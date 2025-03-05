357f9bd285fc6598d9da26dee5a39a225d1102ae
ingemateo30 <msalazar5@udi.edu.co>
2025-03-05 09:01:50 -0500

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+//@type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://jelcom.com.co/',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    outDir: './out',
+  };
+  
